bcbffd25bf2e1a35ef93593b2ac3ee27efb908ca
Ryuji Yamamoto <yamamoto.ryuji.dev@gmail.com>
2020-05-27 22:48:18 +0900

    17で割り切れる整数かどうか判定する関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 'use strict';
 
+/**
+ * 整数が17で割り切れるかどうかを判定する。
+ * @param {integer} int
+ * @return {boolean} 17で割り切れる整数の場合true、それ以外の場合false
+ */
+function isMultipleOfSeventeen(int) {
+    if (!Number.isInteger(int)) {
+        return false;
+    }
+
+    if (int % 17 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 module.exports = {
+    isMultipleOfSeventeen
 }
